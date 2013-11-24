74ecc91011f38aa724ab6b00646f1a70a4e29535
Alice McKay <ajm35@st-andrews.ac.uk>
2013-11-24 23:57:54 +0000

    Update UserProfileServlet.java

--- UserProfileServlet.java
+++ UserProfileServlet.java
@@ -17,7 +17,7 @@ import java.sql.*;
 /**
  * Servlet implementation class UserProfileServlet
  */
-@WebServlet("/all.html")
+@WebServlet("/UserProfileServlet")
 public class UserProfileServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	PrintWriter out;
