8b0da85dbde047cab7a837f30db06786660a0ce8
Alice McKay <ajm35@st-andrews.ac.uk>
2013-11-24 17:59:06 +0000

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -2,10 +2,13 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+// connection to the database
 public class DBConnection {
 
+	// sets up the connection to the database
 	public Connection setConnection(){
 		
+		// gets driver
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			System.out.println("Driver found");
@@ -13,12 +16,15 @@ public class DBConnection {
 		catch (ClassNotFoundException e){
 			System.out.println("Driver not found: " + e);
 		}
+		
+		// stats for the mysql database we are using
 		String url = "jdbc:mysql://daw20.host.cs.st-andrews.ac.uk/daw20_db";
 		String user = "daw20";
 		String password = "Yu!1VrG1";
 		
 		Connection con = null;
 		
+		// tries to connect to the database
 		try {
 			con = DriverManager.getConnection(url, user,password);
 			System.out.println("Connected successfully");
@@ -27,6 +33,7 @@ public class DBConnection {
 			System.out.println("Something went wrong in the connection string");
 		}
 	
+		// returns the connection
 		return con;
 		
 	}
